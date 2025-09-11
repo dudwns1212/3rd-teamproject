7434460027cd6dcdf3e7b8e0d691b609176d435a
ks07235 <amg07235@gmail.com>
2025-09-11 17:50:20 +0900

    김병욱 9/11 postBoad생성  post, hoem 일부 수정 2차

--- gym-homepage-project/src/main/java/lx/gymproject/springboot/dao/GymPostDAO.java
+++ gym-homepage-project/src/main/java/lx/gymproject/springboot/dao/GymPostDAO.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import lx.gymproject.springboot.vo.GymPostVO;
-import lx.gymproject.springboot.vo.UserVO;
 
 @Component
 public class GymPostDAO {
